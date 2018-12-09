45d689829b23948d89f5d648bb2c85fc196ac541
ebarajas1 <ebarajas55@gmail.com>
2018-12-09 15:56:04 -0800

    Add files via upload

---
+++ question_page/assets/js/functions.js
@@ -0,0 +1,19 @@
+
+function button1() {
+    writeToScreen("button 1 was pressed");
+}
+
+
+function button2() {
+    writeToScreen("button 2 was pressed");
+}
+
+
+function button3() {
+    writeToScreen("botton 3 was pressed");
+}
+
+
+function button4() {
+    writeToScreen("button 4 was pressed");
+}
